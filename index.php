53cdf3ac48cef5e3915ee62c220577ddadeca4c4
Johnny Kru <johnnykru@gmail.com>
2017-10-22 18:00:58 -0400

    added img to service

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Johnny Keokongsy's Portfolio </title>
 	<!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link href="css/style.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -29,86 +30,137 @@
 		  <li>Contact</li>
 		 </ul>
 		</div>
-        <div class="main">
-           
-             <div class="bg1">
-			     <div class = "float_text"> 
-							<article>
-							<p id="text">Freelance Web Designer & Developer based in Raleigh,NC. Experienced in HTML5, CSS3, JQuery, JavaScript, Wordpress, PHP  and Mysql websites.</p> 
-							</article>
-							<button id="reset" data-bound="true">Info</button>
-						 </div>		 
-			 </div>
-                 <div class="bio_text">
-				     <p>Learn from everyone. Follow no one. - Unknown. </p><br/> 
-			     </div>
-					<div class="spacer1">
-						 <div class="jk_photo"></div>	
-					     </div>
+             <div class="main">
+                 <div class="bg1">
+								<div class="container">	 
+									<div class="row">
+										 <div class = "float_text col-xsm-12 col-md-6"> 
+											 <article>
+											 <p id="text">" Welcome to my Portfolio page. Scroll down or click menu on top to learn more about me. You will need JavaScript turned on. "</p> 
+											 </article>
+											 <br/>
+											 <button id="reset" data-bound="true"> Reset </button>
+										 </div>	
+									</div>	 
+                                </div>
+								
+					 <br>		
+					 
+			    </div>
+			<div class="spacer1">
+						<div class="johnny_image"><img class="img-circle" src="http://localhost/Portfolio/img/Johnnybw.png"/></div>
+						 <div class="bio_text1"> 
+						 <p> Freelance WebDesigner & Developer based in Raleigh,NC. </p>
+						 <p>Experienced in HTML5, CSS3, JQuery, JavaScript, PHP, and Mysql.</p>
+						 <p>Built and cloned repository using source control software like Git.</p>						 
+						 <p>Maintained and build client websites with WordPress.</p>
+						 <p>Integrated WordPress based websites with other websites.</p> 
+						</div>	
+				<section class="row features">
+					  <div class="col-sm-6 col-md-3">
+						<div class="thumbnail"> 
+							<img src="http://localhost/Portfolio/img/service_01.png" alt="analytics-icon">
+							<div class="caption">
+								<h3>Front End Design</h3>
+								<p>Art has always been part of my life. Watching technology take Art and bring it to life is amazing.</p>
+							</div>
+						</div><! --/thumbnail -->
+					</div><! --/col-sm-6-->
+		 
+					<div class="col-sm-6 col-md-3">
+						<div class="thumbnail"> 
+							<img src="http://localhost/Portfolio/img/service_02.png" alt="analytics-icon">
+							<div class="caption">
+								<h3>Back End </h3>
+								<p>Understadning how things work behind the curtins and collecting information.</p>
+							</div>
+						</div><! --/thumbnail -->
+					</div><! --/col-sm-6-->
+		 
+					<div class="col-sm-6 col-md-3">
+						<div class="thumbnail"> 
+							<img src="http://localhost/Portfolio/img/service_03.png" alt="analytics-icon">
+							<div class="caption">
+								<h3>Putting into Action. </h3>
+								<p>Frameworks helps me get things done quickly. Work alone or work as a team I will enjoy problm solving and learing from others.</p>
+							</div>
+						</div><! --/thumbnail -->
+					</div><! --/col-sm-6-->
+		 
+					<div class="col-sm-6 col-md-3">
+						<div class="thumbnail"> 
+							<img src="http://localhost/Portfolio/img/service_04.png" alt="analytics-icon">
+							<div class="caption">
+								<h3>Agreement</h3>
+								<p>Understanding what is needed as a developer and agreeing to provide professional quality work to our clients.</p>
+							</div>
+						</div><! --/thumbnail -->
+					</div><! --/col-sm-6-->
+				</section><! --/section -->
+            </div>
 					
-						<div data-tilt> <div class="bg2"></div> </div>
+						<div data-tilt><div class="bg2"></div></div>
 						 <div class="photo_wrapper">
-								 <table>
-								 <h1 style="color:white; text-align:center;"> Client Companies :</h1>
-								 </tr>
-								 <td>
-								 <div class="photo1"></div>
-								 </td>
-								 <td>
-								  <div class="photo2"></div>
-								 </td>
-								 </tr>
-								 
-								 <tr>
-								 <td>
-								   <div class="photo3"></div>
-								 </td>
-								 <td>
-								  <div class="photo4"></div>
-								 </td>
-								 </tr>
-								 
-								 </table>
-								 <br>
-							</div>	
+						 <h1>Some of our clients </h1><br><br><br>
+							    <div class="flip3D">
+								  <div class="back">Box 1 - Back</div>
+								  <div class="front"><div class="photo1"></div></div>
+								</div>
+								<div class="flip3D">
+								  <div class="back">Box 2 - Back</div>
+								  <div class="front"> <div class="photo2"></div></div>
+								</div>
+								<div class="flip3D">
+								  <div class="back">Box 3 - Back</div>
+								  <div class="front"><div class="photo3"></div></div>
+								</div><br>
+								<div class="flip3D">
+								  <div class="back">Box 4 - Back</div>
+								  <div class="front"><div class="photo4"></div></div>
+								</div>
+						</div> 
+						 
+				
 							<div class="spacer2">
-							<div class="html5"><p id="icon1">HTML5</p><br></div>
-							<div class="css3"><p id="icon2">css3</p><br></div>
-							<div class="github"><p id="icon3">github</p><br></div>
-							<div class="php7"><p id="icon4"> php / Mysql </p><br></div>
+							<div class="testblock html5"><p id="icon1">HTML5</p><br></div>
+							<div class="testblock css3"><p id="icon2">css3</p><br></div>
+							<div class="testblock github"><p id="icon3">github</p><br></div>
+							<div class="testblock php7"><p id="icon4"> php / Mysql </p><br></div>
+							<div class="testblock wordpress"><p id="icon5"> WordPress </p><br></div>
 						</div>
-							<div data-tilt><div class="bg3"></div></div>
+							<div class="bg3"></div>
 							
 							<div class="spacer3">
-							<form  id="bottom_form1" method="POST" name="contactform" action="http://localhost/Portfolio/mail/thankyou.html">
-<div class="form_wrap">
-<lable for='F_name'>First Name :<font color ="red">*</font></lable>  
-<input style=background:#eee; type="text" id="First_Name">
-</input>
-<br>
-<lable for='L_name'>Last Name :<font color ="red">*</font></lable>
-<input style=background:#eee; type="text" id="Last_Name">
-</input>
-<br>
-<br>
-<lable for='email'> Email:<font color ="red">*</font></lable>   
-<input style=background:#eee; type="text" id="email">
-</input><br>
-<lable for='phone'>Phone:<font color ="red">*</font></lable>
-<input style=background:#eee; type:text id="phone">
-</input><br><br>
-<lable for='subject'>Subject:<font color ="red">*</font></lable>
-<input style=background:#eee; type:text id="subject">
-</input>
-<br>
-<lable for='comment'>Comment:<font color ="red">*</font></lable> 
-<input style=background:#eee; type:text id="comment">
-</input><br>
-<br>
-<br>
-<input type="submit" value="Submit">
-</form>
-</div>
+							
+							<div class="form_wrap">
+								<form  id="bottom_form1" method="POST" name="contactform" action="http://localhost/Portfolio/mail/thankyou.html">
+									<lable for='F_name'>First Name :<font color ="red">*</font></lable>  
+										<input style=background:#eee; type="text" id="First_Name">
+										</input>
+									<br>
+									<lable for='L_name'>Last Name :<font color ="red">*</font></lable>
+										<input style=background:#eee; type="text" id="Last_Name">
+										</input>
+									<br>
+									<br>
+									<lable for='email'> Email:<font color ="red">*</font></lable>   
+										<input style=background:#eee; type="text" id="email">
+										</input><br>
+									<lable for='phone'>Phone:<font color ="red">*</font></lable>
+										<input style=background:#eee; type:text id="phone">
+										</input><br><br>
+									<lable for='subject'>Subject:<font color ="red">*</font></lable>
+										<input style=background:#eee; type:text id="subject">
+										</input>
+									<br>
+									<lable for='comment'>Comment:<font color ="red">*</font></lable> 
+										<input style=background:#eee; type:text id="comment">
+										</input><br>
+									<br>
+									<br>
+										 <input type="submit" value="Submit">
+								</form>
+							</div>
 							
 							</div>
 						
